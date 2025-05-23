a864735550a81a6284a7a6a1f1a0893cb680d70f
ChristofferS22R <christofferdamrong@gmail.com>
2025-05-23 18:55:04 +0200

    Adds 2 items

---
+++ src/main/java/net/damrongmc/glorpcat/item/ModItems.java
@@ -0,0 +1,23 @@
+package net.damrongmc.glorpcat.item;
+
+import net.damrongmc.glorpcat.GlorpCat;
+import net.minecraft.world.item.Item;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.neoforge.registries.DeferredItem;
+import net.neoforged.neoforge.registries.DeferredRegister;
+
+public class ModItems {
+    public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(GlorpCat.MODID);
+
+    public static final DeferredItem<Item> MYSTERIOUSFUNGALTREAT = ITEMS.register("mysterious_fungal_treat",
+            ()-> new Item(new Item.Properties()));
+
+    public static final DeferredItem<Item> GLORPCORE = ITEMS.register("glorp_core",
+            ()-> new Item(new Item.Properties()));
+
+
+    public static void register(IEventBus eventBus) {
+        ITEMS.register(eventBus);
+    }
+
+}
